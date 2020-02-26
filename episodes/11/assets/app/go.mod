ab88f3e821e8f84d842f8bbe1257fc27154cc0cf
Daniel Mangum <31777345+hasheddan@users.noreply.github.com>
2020-02-26 16:32:44 -0600

    Revert "TBS[11] Pre-show notes"

--- episodes/11/assets/app/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/crossplaneio/tbs/episodes/11/assets/app
-
-go 1.13
-
-require (
-	github.com/gorilla/mux v1.7.4
-	github.com/jinzhu/gorm v1.9.12
-)
--- episodes/11/assets/provide.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export BASE64ENCODED_AWS_PROVIDER_CREDS=$(echo -e "[default]\naws_access_key_id = $(aws configure get aws_access_key_id --profile default)\naws_secret_access_key = $(aws configure get aws_secret_access_key --profile default)" | base64  | tr -d "\n")
-sed "s/BASE64ENCODED_AWS_PROVIDER_CREDS/$BASE64ENCODED_AWS_PROVIDER_CREDS/g" aws-provider.yaml | kubectl create -f -
